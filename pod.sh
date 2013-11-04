3e75a70db90e3e0943a60acff560f45c63f874c1
Vegard Øye <vegard_oye@hotmail.com>
2013-11-04 13:35:30 +0100

    updates

--- pod.sh
+++ pod.sh
@@ -15,7 +15,7 @@ then
                         # FAT32 represents time with a 2-second
                         # resolution. Set it to an hour due to
                         # daylight saving regimes.
-			rsync -av --progress --modify-window=4000 --delete "/home/vegard/Musikk/$f" .
+			rsync -av --progress --modify-window=8000 --delete "/home/vegard/Musikk/$f" .
 		fi
 	done
 else
@@ -23,17 +23,17 @@ else
 	# rm -rf "/media/Vegards Pod/Contacts"
 	# rm -rf "/media/Vegards Pod/iPod_Control"
 	# rm -rf "/media/Vegards Pod/Notes"
-	rsync -av --progress --modify-window=4000 --delete \
+	rsync -av --progress --modify-window=8000 --delete \
 	--exclude=".rockbox" --exclude=".scrobbler.log" \
 	--exclude="Playlists" --exclude="playlists" \
 	--exclude="*.m3u" --exclude="*.bmark" --exclude="unsorted" \
 	"/home/vegard/Musikk/" "/media/Vegards Pod"
-	rsync -avz --progress --modify-window=4000 "/home/vegard/Musikk/unsorted" "/media/Vegards Pod"
+	rsync -avz --progress --modify-window=8000 "/home/vegard/Musikk/unsorted" "/media/Vegards Pod"
 	rsync -avz --quiet /media/Vegards\ Pod/*.m3u /media/Vegards\ Pod/Playlists
-	rsync -avz --quiet /media/Vegards\ Pod/*.bmark /media/Vegards\ Pod/Playlists
 	rsync -avz --update --progress /home/vegard/Musikk/playlists/* "/media/Vegards Pod/Playlists"
 	rsync -avz --update --progress /media/Vegards\ Pod/Playlists/*.m3u /home/vegard/Musikk/playlists
 	rsync -avz --progress /home/vegard/Musikk/playlists/* "/media/Vegards Pod"
 	rsync -avz --progress /home/vegard/Musikk/playlists/* "/media/Vegards Pod/Playlists"
+	rsync -avz --quiet /media/Vegards\ Pod/*.bmark /media/Vegards\ Pod/Playlists
 	rsync -avz --quiet "/media/Vegards Pod/.scrobbler.log" /home/vegard/scrobbler.log
 fi
