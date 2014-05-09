c98941c3fdeeead9e42e03fdbc8ec77324a4b9b1
Vegard Øye <vegard_oye@hotmail.com>
2014-05-10 01:17:50 +0200

    libgen

--- libgen.py
+++ libgen.py
@@ -8,7 +8,7 @@ import urlparse
 
 urllib.URLopener.version = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT 5.0; T312461)'
 
-def download(url, path):
+def download(url, path, ask=True):
     """Create download script."""
     script = 'run.sh'
     try:
@@ -18,9 +18,13 @@ def download(url, path):
         output = open(script, 'w')
         try:
             output.write('#!/bin/sh\n')
+            output.write('DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"\n')
+            output.write('cd "$DIR"\n')
             soup = bs4.BeautifulSoup(input)
             table = soup.find('table', 'c')
+            if not table: return
             trs = table.findAll('tr')
+            if not trs: return
             trs.pop(0)
             for tr in trs:
                 tds = tr.findAll('td')
@@ -28,7 +32,7 @@ def download(url, path):
                 link = urlparse.urljoin(url,
                                        tds[9].find('a')['href'])
                 ext = tds[8].get_text()
-                wget = 'wget -t inf -T 10 -O %s.%s "%s"\n' % (id, ext, link)
+                wget = 'wget -c -w 60 -t inf -T 10 -O "%s %s.%s" "%s"\n' % (id, path, ext, link)
                 print(wget)
                 output.write(wget)
         finally:
@@ -37,11 +41,14 @@ def download(url, path):
     except IOError:
         return
     os.system('chmod +x "%s"' % script)
-    os.system('./%s' % script)
+    if ask and raw_input("Start downloading? (Y/N) ").upper() == 'Y':
+        os.system('./%s' % script)
 
 def main():
     path = sys.argv[1]
-    url = sys.argv[2]
+    url = (sys.argv[2] if len(sys.argv) > 2 else
+           'http://gen.lib.rus.ec/search.php?req=%s' %
+           urllib.quote_plus(path))
     download(url, path)
 
 if __name__ == '__main__':
