96f2b15dfa2b39a038bdbd2ba10e664b8faff09f
Vegard Øye <vegard_oye@hotmail.com>
2016-06-14 13:29:32 +0200

    echo

--- html2md.sh
+++ html2md.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 name=$1
-pandoc -f html -t markdown --columns=1000 --wrap=none -o "${name%.html}.md" "$name"
+pandoc -f html -t markdown --columns=1000 --wrap=none -o "${name%.html}.md" "$name" && echo "Converted $name to ${name%.html}.md"
