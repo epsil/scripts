ccac68f8009906fcaef3bd6550447935ec016bb7
Vegard Øye <vegard_oye@hotmail.com>
2017-07-30 00:40:58 +0200

    functions

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+PARAM=$1
 HOME=/Users/vegard
 BACKUP=/Volumes/Backup
 WINDOW=3600
@@ -8,31 +9,41 @@ ARCHIVE="-avz $PARAMS"
 CLOUD="vegardye@ananke.feralhosting.com:www/vegardye.ananke.feralhosting.com/public_html"
 
 # Preparations
-echo "Preparing backup ..."
-rsync $ARCHIVE $HOME/wiki/leseliste/index.md $HOME/GoogleDrive/Calibre\ Library/Ukjent/Leseliste\ \(1700\)/Leseliste\ -\ Ukjent.txt
+function prepare {
+    echo "Preparing backup ..."
+    rsync $ARCHIVE $HOME/wiki/leseliste/index.md $HOME/GoogleDrive/Calibre\ Library/Ukjent/Leseliste\ \(1700\)/Leseliste\ -\ Ukjent.txt
+}
 
 # Disk backup
-if [ ! -e "$BACKUP" ]; then
-    echo "Backup disk not found!"
-else
-    echo "Backing up to disk ..."
-    rsync $DELETE $HOME/Music $BACKUP
-    rsync $DELETE $HOME/GoogleDrive/Calibre\ Library $BACKUP
-    rsync $DELETE $HOME/GoogleDrive/Documents $BACKUP
-    rsync $ARCHIVE $HOME/wiki $BACKUP
-    rsync $ARCHIVE $HOME/Dropbox $BACKUP
-    rsync $ARCHIVE $HOME/Movies $BACKUP
-    rsync $ARCHIVE $HOME/Downloads $BACKUP
-    rsync $ARCHIVE $HOME/GoogleDrive/Calibre\ Library $BACKUP/Downloads
-    rsync $ARCHIVE $HOME/Music $BACKUP/Downloads
-    rsync $ARCHIVE $HOME/Documents/scripts/backup.sh $BACKUP
-fi
+function disk {
+    if [ ! -e "$BACKUP" ]; then
+        echo "Backup disk not found!"
+    else
+        echo "Backing up to disk ..."
+        rsync $DELETE $HOME/Music $BACKUP
+        rsync $DELETE $HOME/GoogleDrive/Calibre\ Library $BACKUP
+        rsync $DELETE $HOME/GoogleDrive/Documents $BACKUP
+        rsync $ARCHIVE $HOME/wiki $BACKUP
+        rsync $ARCHIVE $HOME/Dropbox $BACKUP
+        rsync $ARCHIVE $HOME/Movies $BACKUP
+        rsync $ARCHIVE $HOME/Downloads $BACKUP
+        rsync $ARCHIVE $HOME/GoogleDrive/Calibre\ Library $BACKUP/Downloads
+        rsync $ARCHIVE $HOME/Music $BACKUP/Downloads
+        rsync $ARCHIVE $HOME/Documents/scripts/backup.sh $BACKUP
+    fi
+}
 
 # Cloud backup
-echo "Backing up to cloud ..."
-# rsync $DELETE $HOME/GoogleDrive/ $CLOUD
-rsync $DELETE $HOME/GoogleDrive/Calibre\ Library/ "$CLOUD/Calibre\ Library"
-# rsync $DELETE $HOME/GoogleDrive/Documents/ $CLOUD/Documents
-rsync $DELETE $HOME/wiki/ $CLOUD/wiki
-# rsync $ARCHIVE $HOME/Downloads/stuff/stuff/ $CLOUD/stuff/stuff
-echo "Backup completed"
+function cloud {
+    echo "Backing up to cloud ..."
+    # rsync $DELETE $HOME/GoogleDrive/ $CLOUD
+    rsync $DELETE $HOME/GoogleDrive/Calibre\ Library/ "$CLOUD/Calibre\ Library"
+    # rsync $DELETE $HOME/GoogleDrive/Documents/ $CLOUD/Documents
+    rsync $DELETE $HOME/wiki/ $CLOUD/wiki
+    # rsync $ARCHIVE $HOME/Downloads/stuff/stuff/ $CLOUD/stuff/stuff
+    echo "Backup completed"
+}
+
+prepare
+cloud
+disk
