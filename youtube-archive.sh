ecb1750aef88eba8a42c0c027eefcd18340a3e65
Vegard Øye <vegard_oye@hotmail.com>
2018-08-12 20:10:29 +0200

    use mp4

--- youtube-archive.sh
+++ youtube-archive.sh
@@ -2,7 +2,8 @@
 URL=$1
 YOUTUBEDL="youtube-dl"
 OPTs="--add-metadata --embed-thumbnail --all-subs --embed-subs --sub-format srt"
-MP4="$YOUTUBEDL --add-metadata --embed-thumbnail --all-subs --embed-subs --sub-format srt --merge-output-format mp4 $URL"
+MP4="$YOUTUBEDL -f mp4 --add-metadata --embed-thumbnail --all-subs --embed-subs --sub-format srt --merge-output-format mp4 $URL"
 MKV="$YOUTUBEDL $OPTS --merge-output-format mkv $URL"
 DEF="$YOUTUBEDL $OPTS $URL"
-$DEF
+# $DEF
+$MP4
