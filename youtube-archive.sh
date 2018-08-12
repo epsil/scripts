3dd0e7be1c146d96edcbf717f2950df685668b79
Vegard Øye <vegard_oye@hotmail.com>
2018-08-12 20:03:38 +0200

    adjustments

--- youtube-archive.sh
+++ youtube-archive.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 URL=$1
-youtube-dl --add-metadata --embed-thumbnail --all-subs --embed-subs --sub-format srt "$URL"
+YOUTUBEDL="youtube-dl"
+OPTs="--add-metadata --embed-thumbnail --all-subs --embed-subs --sub-format srt"
+MP4="$YOUTUBEDL --add-metadata --embed-thumbnail --all-subs --embed-subs --sub-format srt --merge-output-format mp4 $URL"
+MKV="$YOUTUBEDL $OPTS --merge-output-format mkv $URL"
+DEF="$YOUTUBEDL $OPTS $URL"
+$DEF
