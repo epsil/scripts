805110dabfc0d6adf2db6411f0e26c9098e4fa09
Vegard Øye <vegard_oye@hotmail.com>
2019-01-23 10:51:24 +0100

    meta

---
+++ meta/index.js
@@ -0,0 +1,9 @@
+// Transpile all code following this line with babel
+// and use 'env' (aka ES6) preset
+require('@babel/register')({
+  presets: ['@babel/preset-env']
+});
+require('@babel/polyfill');
+
+// Import the rest of the application
+require('./main.js');
