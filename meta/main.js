1f71b040d8ccc89092b36f14fc517692511b11a8
Vegard Øye <vegard_oye@hotmail.com>
2019-02-12 09:33:39 +0100

    install glob, prettier and eslint

--- meta/main.js
+++ meta/main.js
@@ -1,5 +1,15 @@
+import glob from 'glob';
+
+function findAllMetaFiles() {
+  const files = glob
+    .sync('**/.meta/*.yml', { dot: true, ignore: 'node_modules/**' })
+    .sort();
+  console.log(files);
+}
+
 function main() {
   console.log('Hello world!');
+  findAllMetaFiles();
 }
 
 main();
