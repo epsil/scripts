88ce31e3a94cfa57c9435a59531e048780095cce
Vegard Øye <vegard_oye@hotmail.com>
2019-03-02 01:58:41 +0100

    json

--- youtube-archive.sh
+++ youtube-archive.sh
@@ -10,8 +10,8 @@ fi
 for URL in "$URLS"
 do
     YOUTUBEDL="youtube-dl"
-    OPTs="--add-metadata --embed-thumbnail --all-subs --embed-subs --sub-format srt"
-    MP4="$YOUTUBEDL -f mp4 --add-metadata --embed-thumbnail --all-subs --embed-subs --sub-format srt --merge-output-format mp4 $URL"
+    OPTs="--add-metadata --embed-thumbnail --all-subs --embed-subs --sub-format srt --write-info-json"
+    MP4="$YOUTUBEDL -f mp4 --add-metadata --embed-thumbnail --all-subs --embed-subs --sub-format srt --write-info-json --merge-output-format mp4 $URL"
     MKV="$YOUTUBEDL $OPTS --merge-output-format mkv $URL"
     DEF="$YOUTUBEDL $OPTS $URL"
     # $DEF
